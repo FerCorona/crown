45e94d1a36497e840b991b8645024f916cbd7696
Fernando Garcia Corona <42970838+FerCorona@users.noreply.github.com>
2020-08-29 17:27:03 -0500

    Button Clompleted

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,19 +1,51 @@
-
-import React from 'react';
+import React, { Component } from 'react';
 
 require('./styles.scss');
 
-const Button = ({ text }) => {
-  return (
-    <a
-      className="CROWNButton"
-    >
-    {text}
-    </a>);
+class Button extends Component {
+  constructor() {
+    super();
+    this.state = { };
+  }
+
+  render() {
+    const { text, onClick, color, textColor, borderColor, type, shape } = this.props;
+    const style = {};
+    switch (type) {
+      case 'Secondary':
+        style.border = ` 2px solid var(${borderColor})`;
+        style.color = `var(${textColor})`;
+        break;
+      case 'Text':
+        style.border = 'none';
+        style.color = `var(${textColor})`;
+        style.boxShadow = 'none';
+        break;
+      default:
+        style.backgroundColor = `var(${color})`;
+        style.color = `var(${textColor})`;
+        break;
+    };
+    return (
+      <a
+        className={`CROWNButton ${shape}`}
+        style={style}
+        onClick={onClick}
+      >
+        {text}
+      </a>
+    );
+  }
 }
 
 Button.defaultProps = {
-  text: 'Click Me!'
+  text: 'Click Me!',
+  onClick: () => console.log('You clicked on me!'),
+  color: '--blue-4',
+  textColor: '--white',
+  borderColor: '--blue-4',
+  type: 'Default',
+  shape: 'Round'
 };
 
 export default Button;
