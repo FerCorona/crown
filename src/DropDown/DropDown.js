3c7b36e20d169d49ba4687ec82cd3f90c2bc5be8
Fernando Garcia Corona <42970838+FerCorona@users.noreply.github.com>
2020-12-21 21:15:28 -0600

    Fix DropDow

--- src/DropDown/DropDown.js
+++ src/DropDown/DropDown.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const DropDown = ({ optionSelected, options, onChange, stateName }) => (
-  <div className='CROWNDropDown'>
-    <div className='Options'>
-      {optionSelected}
-    </div>
-    <div className='ListContent'>
-      {
-        options.map((option, index) => (
-          <div
-            className='Option'
-            key={`option-${index}`}
-            onClick={() => onChange({
-              stateName,
-              value: option
-            })}
-          >
-            {option}
-          </div>
-        ))
-      }
-    </div>
-  </div>
-);
+class DropDown extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isActive: false
+    };
+    this._toggleActive = this._toggleActive.bind(this);
+  }
+
+  _toggleActive() {
+    this.setState({ isActive: !this.state.isActive });
+  }
+
+  render() {
+    const { optionSelected, options, onChange, stateName } = this.props;
+    return (
+      <div className='CROWNDropDown'>
+        <div className='Options' onClick={() => this._toggleActive()}>
+          {optionSelected}
+        </div>
+        {
+          this.state.isActive && (
+            <div className='ListContent'>
+              {
+                options.map((option, index) => (
+                  <div
+                    className='Option'
+                    key={`option-${index}`}
+                    onClick={() => this.setState({ isActive: false },
+                      onChange({
+                        stateName,
+                        value: option
+                      }))}
+                  >
+                    {option}
+                  </div>
+                ))
+              }
+            </div>
+          )
+        }
+      </div>
+    );
+  }
+}
 
 DropDown.defaultProps = {
-  options: ['Button', 'Common', 'DropDown', 'Image', 'InputText', 'Label', 'RadioButton', 'Switch'],
+  options: [ 'Button', 'Common', 'DropDown', 'Image', 'InputText', 'Label', 'RadioButton', 'Switch' ],
   placeholder: 'Selecciona una opcion',
-  onChange: () => {}
+  onChange: () => { }
 };
 
 export default DropDown;
